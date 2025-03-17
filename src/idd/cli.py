compiler-research/idd

commit 42a78252d00b3b1211df562fc06e281dd400a8a1

feat: reorg as package (#41)
Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

src/idd/cli.py
@@ -10,11 +10,11 @@ from textual.reactive import Reactive
 from textual.widgets import Input
 from textual.containers import Horizontal, Vertical
 
-from diff_driver import DiffDriver
+from idd.diff_driver import DiffDriver
 
-from ui.footer import Footer
-from ui.header import Header
-from ui.scrollable_area import TextScrollView
+from idd.ui.footer import Footer
+from idd.ui.header import Header
+from idd.ui.scrollable_area import TextScrollView
 
 
 class DiffDebug(App):
@@ -413,7 +413,7 @@ class DiffDebug(App):
             self.regressed_command_bar.value = self.regressed_history[self.regressed_history_index]
 
 
-if __name__ == "__main__":
+def main() -> None:
     Debugger = None
 
     parser = argparse.ArgumentParser(description='Diff Debug for simple debugging!')
@@ -446,7 +446,7 @@ if __name__ == "__main__":
     base_only = False
 
     if comparator == 'gdb':
-        from debuggers.gdb.gdb_mi_driver import GDBMiDebugger, IDDGdbController
+        from idd.debuggers.gdb.gdb_mi_driver import GDBMiDebugger, IDDGdbController
 
         if ra == "" and rpid is None:
             Debugger = IDDGdbController(ba, bpid, bs)
@@ -455,7 +455,7 @@ if __name__ == "__main__":
             Debugger = GDBMiDebugger(ba, bs, ra, rs, base_pid=bpid, regression_pid=rpid)
 
     elif comparator == 'lldb':
-        from debuggers.lldb.lldb_driver import LLDBParallelDebugger, LLDBDebugger
+        from idd.debuggers.lldb.lldb_driver import LLDBParallelDebugger, LLDBDebugger
 
         if ra == "" and rpid is None:
             Debugger = LLDBDebugger(ba, bpid)

pyproject.toml
@@ -1,0 +1,24 @@
+[build-system]
+requires = ["hatchling"]
+build-backend = "hatchling.build"
+
+[project]
+name = "idd"
+version = "0.1.0"
+description = "Add your description here"
+readme = "README.md"
+authors = [
+    { name = "Martin Vassilev" },
+    { name = "Vassil Vassilev" }, 
+    { name = "Alexander Penev" },
+]
+requires-python = ">=3.9"
+dependencies = [
+  "pygdbmi",
+  "rich",
+  "textual",
+]
+
+
+[project.scripts]
+idd = "idd.cli:main"

src/idd/__main__.py
@@ -1,0 +1,4 @@
+from idd.cli import main
+
+if __name__ == "__main__":
+    main()

src/idd/debuggers/gdb/gdb_driver.py
@@ -1,6 +1,6 @@
 from pygdbmi.gdbcontroller import GdbController
 from pprint import pprint
-from driver import Driver
+from idd.driver import Driver
 
 import io
 import time

src/idd/debuggers/gdb/gdb_mi_driver.py
@@ -1,10 +1,10 @@
 import os
 import json
 
-from debuggers.gdb.idd_gdb_controller import create_IDDGdbController, terminate_all_IDDGdbController, IDDGdbController
-from driver import Driver
+from idd.debuggers.gdb.idd_gdb_controller import create_IDDGdbController, terminate_all_IDDGdbController, IDDGdbController
+from idd.driver import Driver
 
-from debuggers.gdb.utils import parse_gdb_line
+from idd.debuggers.gdb.utils import parse_gdb_line
 
 base_response = []
 regressed_response = []

src/idd/debuggers/gdb/idd_gdb_controller.py
@@ -3,8 +3,8 @@ import logging
 import subprocess
 import json
 
-from driver import IDDParallelTerminate
-from debuggers.gdb.utils import parse_gdb_line
+from idd.driver import IDDParallelTerminate
+from idd.debuggers.gdb.utils import parse_gdb_line
 
 from pygdbmi.gdbcontroller import GdbController
 from pygdbmi.IoManager import IoManager

commit 21dbaa2a6c03419bd678ef930fa52d76cc21af3c

fix: bugs introduced with the #41 rebase
Previously `disable_assembly`, `Debugger` & `disable_registers` were globals.

src/idd/cli.py
@@ -67,9 +67,9 @@ class DiffDebug(App):
         await self.set_pframes_result(state, version)
         await self.set_pargs_result(state, version)
         await self.set_plocals_result(state, version)
-        if not disable_assembly:
+        if not self.disable_asm:
             await self.set_pasm_result(state, version)
-        if not disable_registers:
+        if not self.disable_registers:
             await self.set_pregisters_result(state, version)
 
     async def set_common_command_result(self, command_result) -> None:
@@ -84,9 +84,9 @@ class DiffDebug(App):
             await self.set_pframes_command_result(state)
             await self.set_pargs_command_result(state)
             await self.set_plocals_command_result(state)
-            if not disable_assembly:
+            if not self.disable_asm:
                 await self.set_pasm_command_result(state)
-            if not disable_registers:
+            if not self.disable_registers:
                 await self.set_pregisters_command_result(state)
 
             #calls = Debugger.get_current_calls()
@@ -413,8 +413,9 @@ class DiffDebug(App):
             self.regressed_command_bar.value = self.regressed_history[self.regressed_history_index]
 
 
+Debugger = None
 def main() -> None:
-    Debugger = None
+    global Debugger
 
     parser = argparse.ArgumentParser(description='Diff Debug for simple debugging!')
 